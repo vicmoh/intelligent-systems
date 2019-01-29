f3615f2de608ad25b130ea1637afeaef4b7afe2f
Vicky Mohammad <vimoh1213@gmail.com>
2019-01-29 15:30:42 -0500

    [AUTO]

--- makefile
+++ makefile
@@ -1,7 +1,12 @@
+program: compile run
+
+compile:
+	javac -Xlint:unchecked ./src/*.java -d ./bin/
+
+run:
+	java -cp ./bin Main
+
 git:
 	git add -A
 	git commit -m "[AUTO]"
 	git push
-
-compile:
-	javac ./src/*.java
---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.util.Scanner;
+import java.util.HashMap;
+import java.util.ArrayList;
+
+
+public class Main{
+    public static void main(String args[]){
+        System.out.println("BROOOOOOOO");
+        System.out.println("Test");
+        System.out.println("Test");
+        System.out.println("Test");
+        System.out.println("Test");
+    }
+}
