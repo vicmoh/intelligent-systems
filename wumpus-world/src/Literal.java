0df8e57488e7e3181e5714b4e2fc21c0e46c05b7
Vicky Mohammad <vimoh1213@gmail.com>
2019-03-17 04:27:43 -0400

    [AUTO]

--- wumpus-world/src/Literal.java
+++ wumpus-world/src/Literal.java
@@ -7,4 +7,27 @@ public class Literal {
     private Symbol symbol; // a propositon symbol
     private String symbolSign; // a sign of proposition symbol + or -
     private boolean sign; // Every symbol must have a sign. It is a negative or positive sign.
+
+    /**
+     * @param symbol a propositon symbol
+     * @param symbolSign a sign of proposition symbol + or -
+     * @param sign Every symbol must have a sign. It is a negative or positive sign.
+     */
+    Literal(Symbol symbol, String symbolSign, boolean sign){
+        this.symbol = symbol;
+        this.symbolSign = symbolSign;
+        this.sign = sign;
+    }//end constructor
+
+    Symbol getSymbol(){
+        return this.symbol;
+    }//end func
+
+    String getSymbolSign(){
+        return this.symbolSign;
+    }//end func
+
+    boolean getSign(){
+        return this.sign;
+    }//end func
 }// end class
