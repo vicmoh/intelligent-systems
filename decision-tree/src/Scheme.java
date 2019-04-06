5921017e330b57514bd3e1a6f0890bed1ea1eafd
Vicky Mohammad <vimoh1213@gmail.com>
2019-04-06 13:47:24 -0400

    [AUTO]

--- decision-tree/src/Scheme.java
+++ decision-tree/src/Scheme.java
@@ -8,45 +8,49 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- * Scheme class provides the definition of attributes and a function related to data file.
- * It stores scheme file attributes and the function
+ * Scheme class provides the definition of attributes and a function related to
+ * data file. It stores scheme file attributes and the function
+ * 
  * @author Vicky Mohammad
  */
-public class Scheme {  
-    ArrayList <Attribute> attributeList; // attribute list of scheme file
+public class Scheme {
+    ArrayList<Attribute> attributeList; // attribute list of scheme file
     int numberOfAttribute = 0; // number of attributes
     Attribute function = null; // a function
-    
+
     /**
      * Creates scheme object
      */
-    public Scheme(){
+    public Scheme() {
         attributeList = new ArrayList<Attribute>();
-    }//End constructor
+    }// End constructor
 
     /**
      * Load the scheme file
+     * 
      * @param schemeFile string of the path
      */
-    public void loadSchemeFile(String schemeFile){
-        
+    public void loadSchemeFile(String schemeFile) {
+
     }// End function
 
     /**
      * Print thee Scheme
      */
-    public void printScheme(){
-        attributeList.forEach((k)->{ k.printAttribute();});
+    public void printScheme() {
+        attributeList.forEach((k) -> {
+            k.printAttribute();
+        });
         System.out.println(numberOfAttribute);
-        if(function == null) {
+        if (function == null) {
             System.out.println("Function: null");
-        }else{
+        } else {
             System.out.println("Function: ");
             function.printAttribute();
-        }// End if
+        } // End if
     }// End function
 
-    public static void main(String [] args){
+    public static void main(String[] args) {
         Scheme s = new Scheme();
         s.loadSchemeFile("GolfScheme.txt");
         s.printScheme();
